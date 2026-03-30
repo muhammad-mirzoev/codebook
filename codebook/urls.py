57dbf447ebc48b7fb2513c2936b48983851336f8
mac <237774366+shumsterm406-oss@users.noreply.github.com>
2026-03-31 01:28:37 +0600

    add apps/books in settings, urls

--- codebook/urls.py
+++ codebook/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('apps.books.urls', namespace='books')),
     path('users/', include('apps.users.urls', namespace='users')),
 ]
 
